a5521127ce15ef60dfa47a56516c0896d0176252
bingxindan <543802214@qq.com>
2021-11-19 21:49:16 +0800

    log4-init

---
+++ db/db.go
@@ -0,0 +1,134 @@
+package db
+
+import (
+	"context"
+	"github.com/bingxindan/bxd_go_lib/tools/confutil"
+	"log"
+	"strings"
+	"sync/atomic"
+	"time"
+
+	"github.com/spf13/cast"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+)
+
+type DBDao struct {
+	Engine *Engine
+	quiter chan struct{}
+}
+
+var (
+	db_instance  map[string][]*DBDao
+	curDbPoses   map[string]*uint64 // 当前选择的数据库
+	showSql      bool
+	showExecTime bool
+	slowDuration time.Duration
+	maxConn      int = 100
+	maxIdle      int = 30
+)
+
+func newDBDaoWithParams(host string, driver string) (Db *DBDao) {
+	Db = new(DBDao)
+
+	eng, err := xorm.NewEngine(driver, host)
+	engine := &Engine{Engine: eng}
+
+	Db.Engine = engine
+	//TODO: 增加存活检查
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*Db.Engine.Logger.SetLevel(core.LOG_DEBUG)
+	  Db.Engine.ShowSQL = true
+	  Db.Engine.ShowInfo = true
+	  Db.Engine.ShowDebug = true
+	  Db.Engine.ShowErr = true
+	  Db.Engine.ShowWarn = true*/
+	Db.Engine.SetMaxOpenConns(maxConn)
+	Db.Engine.SetMaxIdleConns(maxIdle)
+	Db.Engine.SetConnMaxLifetime(time.Second * 3000)
+	Db.Engine.ShowSQL(showSql)
+	Db.Engine.ShowExecTime(showExecTime)
+	Db.Engine.SetLogger(dbLogger)
+	return
+}
+
+func GetDefault(cluster string) *DBDao {
+	return GetDbInstance("default", cluster)
+}
+
+func init() {
+	db_instance = make(map[string][]*DBDao, 0)
+	curDbPoses = make(map[string]*uint64)
+	//idc := confdao.GetIDC()
+	idc := ""
+	showLog := confutil.GetConfStringMap("MysqlConfig")
+	showSql = showLog["showSql"] == "true"
+	showExecTime = showLog["showExecTime"] == "true"
+	slowDuration = time.Duration(cast.ToInt(showLog["slowDuration"])) * time.Millisecond
+	maxConnConfig := cast.ToInt(showLog["maxConn"])
+	if maxConnConfig > 0 {
+		maxConn = maxConnConfig
+	}
+	maxIdleConfig := cast.ToInt(showLog["maxIdle"])
+	if maxIdleConfig > 0 {
+		maxIdle = maxIdleConfig
+	}
+	if maxIdle > maxConn {
+		maxIdle = maxConn
+	}
+	for cluster, hosts := range confutil.GetConfArrayMap("MysqlCluster") {
+		items := strings.Split(cluster, ".")
+		//必须包含 writer 和 reader
+		if len(items) < 2 {
+			continue
+		}
+		//过滤IDC
+		if len(items) > 2 && items[2] != idc {
+			continue
+		}
+		instance := items[0] + "." + items[1]
+		dbs := make([]*DBDao, 0)
+		for _, host := range hosts {
+			dbs = append(dbs, newDBDaoWithParams(host, "mysql"))
+		}
+		db_instance[instance] = dbs
+		curDbPoses[instance] = new(uint64)
+	}
+}
+
+func GetDbInstance(db, cluster string) *DBDao {
+	key := db + "." + cluster
+	if instances, ok := db_instance[key]; ok {
+		// round-robin选择数据库
+		cur := atomic.AddUint64(curDbPoses[key], 1) % uint64(len(instances))
+		return instances[cur]
+	} else {
+		return nil
+	}
+}
+
+func GetDbInstanceWithCtx(ctx context.Context, db, cluster string) *DBDao {
+	bench := ctx.Value("IS_BENCHMARK")
+	if cast.ToString(bench) == "1" {
+		db = "benchmark_" + db
+	}
+	key := db + "." + cluster
+	if instances, ok := db_instance[key]; ok {
+		// round-robin选择数据库
+		cur := atomic.AddUint64(curDbPoses[key], 1) % uint64(len(instances))
+		return instances[cur]
+	} else {
+		return nil
+	}
+}
+
+func (this *DBDao) GetSession() *Session {
+	return this.Engine.NewSession()
+}
+
+func (this *DBDao) Close() {
+	this.Engine.Close()
+}
---
+++ logger/accessLogLib.go
@@ -0,0 +1,43 @@
+package logger
+
+import (
+	"fmt"
+	"xxx/builders"
+	"github.com/sirupsen/logrus"
+	"go.uber.org/zap"
+	"os"
+)
+
+/*
+
+方法：AccessLogLib 接入loglib
+功能：接入多样logLib 以支持其他组件中 loggerX 日志打印格式
+@param libName 接入logLib库名 默认接入log4go
+@param args[0] logrus全局logger实例 使用默认的logrus Builder
+@param args[0] zap全局logger实例 使用默认的zap Builder
+*/
+
+func AccessLogLib(libName string, args interface{}) {
+
+	if libName == "logrus" {
+		instance, ok := args.(*logrus.Logger)
+		if !ok {
+			fmt.Fprintf(os.Stderr, "SetLogLib Error: Could not get logrus instance!")
+			os.Exit(1)
+		}
+		libBuild := builders.NewLogrusBuilder(instance)
+		SetBuilder(libBuild)
+	} else if libName == "zap" {
+
+		instance, ok := args.(*zap.Logger)
+		if !ok {
+			fmt.Fprintf(os.Stderr, "SetLogLib Error: Could not get zap instance!")
+			os.Exit(1)
+		}
+		libBuild := builders.NewZapBuilder(instance)
+		SetBuilder(libBuild)
+	} else {
+		libName = "log4go"
+	}
+	return
+}
