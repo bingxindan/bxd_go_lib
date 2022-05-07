0986485caf74f0c1135f546d644da8ef24e8093b
bingxindan <543802214@qq.com>
2022-05-07 18:28:06 +0800

    获取服务器IP

--- helper/helper.go
+++ helper/helper.go
@@ -1,7 +1,11 @@
 package helper
 
 import (
+	"context"
+	"github.com/bingxindan/bxd_go_lib/logger"
+	"github.com/pkg/errors"
 	"math/rand"
+	"net"
 	"strconv"
 	"time"
 )
@@ -84,4 +88,26 @@ func GetRandomString(length int) string {
 		result = append(result, bytes[r.Intn(len(bytes))])
 	}
 	return string(result)
+}
+
+// desc 获取服务器IP
+// @author zhangming
+// @date 2022/5/7-18:15
+func GetSrvIP(ctx context.Context) (ip string, err error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		logger.Ex(ctx, "helper.GetSrvIP", "net.InterfaceAddrs fail, err: %+v", err)
+		return "", err
+	}
+
+	for _, addr := range addrs {
+		// 检查ip地址判断是否回环地址
+		if inet, ok := addr.(*net.IPNet); ok && !inet.IP.IsLoopback() {
+			if inet.IP.To4() != nil {
+				return inet.IP.String(), nil
+			}
+		}
+	}
+
+	return "", errors.New("服务器IP获取失败！")
 }
