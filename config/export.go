335747d1d901a27c09d4ebbca516e1148927e26b
zhangming03 <zhangming03@qianxin.com>
2022-10-06 02:07:18 +0800

    db

--- config/export.go
+++ config/export.go
@@ -3,6 +3,7 @@ package config
 import (
 	"context"
 	"sync"
+	"time"
 )
 
 type ExportConfig Config
@@ -40,6 +41,62 @@ func GlobalConfig() ExportConfig {
 	return defaultConfig
 }
 
+func Bool(key string) bool {
+	return GlobalConfig().Bool(key)
+}
+
+func Int(key string) int {
+	return GlobalConfig().Int(key)
+}
+
+func Uint(key string) uint {
+	return GlobalConfig().Uint(key)
+}
+
+func Int64(key string) int64 {
+	return GlobalConfig().Int64(key)
+}
+
+func Uint64(key string) uint64 {
+	return GlobalConfig().Uint64(key)
+}
+
+func Float64(key string) float64 {
+	return GlobalConfig().Float64(key)
+}
+
+func Duration(key string) time.Duration {
+	return GlobalConfig().Duration(key)
+}
+
+func Bytes(key string) []byte {
+	return GlobalConfig().Bytes(key)
+}
+
 func String(key string) string {
 	return GlobalConfig().String(key)
+}
+
+func Int64List(key string) []int64 {
+	return GlobalConfig().Int64List(key)
+}
+
+func Uint64List(key string) []uint64 {
+	return GlobalConfig().Uint64List(key)
+}
+
+func StringList(key string) []string {
+	return GlobalConfig().StringList(key)
+}
+
+func StringMap(key string) map[string]string {
+	return GlobalConfig().StringMap(key)
+}
+
+func GetByPrefix(prefix string) map[string]string {
+	return GlobalConfig().GetByPrefix(prefix)
+}
+
+func Has(key string) bool {
+	return GlobalConfig().Has(key)
 }
